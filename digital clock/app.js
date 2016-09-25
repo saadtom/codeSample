df4a0ee2b3b8e68924ef0d41bb190e5e69dd6ec9
Tommy Tomaye <Tommy Tomaye>
2016-09-25 15:51:49 -0400

    showing 2 digits for hours/minutes/seconds

--- digital clock/app.js
+++ digital clock/app.js
@@ -4,9 +4,9 @@ function clock() {
 	var year = someDate.getFullYear(); 
 	var month = someDate.getMonth() + 1;
 	var day = someDate.getDate(); 
-	var hours = someDate.getHours();
-	var minutes = someDate.getMinutes(); 
-	var seconds = someDate.getSeconds(); 
+	var hours = someDate.getHours(); 
+	var minutes = ("0" + someDate.getMinutes()).slice(-2); // here we are showing 2 digits for minutes
+	var seconds = ("0" + someDate.getSeconds()).slice(-2); // here we are showing 2 digits for seconds
 	var morningOrNoon = "AM"; 
 	var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -16,6 +16,7 @@ function clock() {
 	// if this is afternoon time the clock should show PM 
 	if (hours > 12) {
 		hours = hours - 12; 
+		hours = ("0" + hours).slice(-2); // here we are showing 2 digits for the hours like 03:00:00
 		morningOrNoon = "PM"; 
 	} else if (hours == 12 ) {
 		morningOrNoon = "PM"; 
@@ -28,7 +29,7 @@ function clock() {
 	var timeParagraph = document.createElement("h1");
 	var ampm = document.createElement("h1");
 	    timeParagraph.innerHTML = currentTime; 
-	    timeParagraph.style.width = "250px"; 
+	    timeParagraph.style.width = "300px"; 
 	    ampm.innerHTML = morningOrNoon;
 		$("#clock").append(timeParagraph);
 		$("#clock").append(ampm);
